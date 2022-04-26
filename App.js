04372585e1cf7a3040f6478bb19cd066f777b704
Djoeke <djoekeduijker@live.nl>
2022-04-27 00:10:38 +0200

    First commit and setup

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react"
+//Ik weet nog niet of we react moeten gebruiken voor de front end
+export default function App() {
+    return (
+        <>
+            <h1>Hello this is our app and it works</h1>
+        </>
+    )
+}
