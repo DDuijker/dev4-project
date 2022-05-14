f7a54920fd0b7314c14ed5c93319039d6360603d
Djoeke <djoekeduijker@live.nl>
2022-05-14 17:33:46 +0200

    home page

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "../css/home.css"
 
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <div className={"banner"}>
+                <h1 className={"banner--header"}>We love good food.</h1>
+            </div>
+            <div className={"about-us"}>
+                <h3 className={"about-us--header-text"}>Over ons:</h3>
+                <p className={"about-us--text"}>Gelegen aan de herengracht in Amsterdam
+                    met prachtig uitzicht op de grachten.
+                    We zijn een apart eetcafe waar je ook nog kan gamen en coderen:
+                    nieuw en revolutionair.
+                    Geniet van heerlijk eten met een enorme keuze aan dranken
+                    U bent van harte welkom om te komen lunchen of dineren.</p>
+            </div>
         </div>
     )
 }
