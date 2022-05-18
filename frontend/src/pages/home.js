58b127007bd912981b9af7deb6920022cf9d4ab7
Floor K <98243636+killingFloor5@users.noreply.github.com>
2022-05-18 10:49:44 +0200

    verbetering menu enzo

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -17,7 +17,7 @@ export default function Home() {
         </Link>
       </div>
       <div className={"about-us"}>
-        <h3 className={"about-us--header-text"}>Over ons:</h3>
+        <h2 className={"about-us--header-text"}>Over ons:</h2>
         <p className={"about-us--text"}>
           Gelegen aan de herengracht in Amsterdam met prachtig uitzicht op de
           grachten. We zijn een apart eetcafe waar je ook nog kan gamen en
@@ -25,7 +25,7 @@ export default function Home() {
           enorme keuze aan dranken U bent van harte welkom om te komen lunchen
           of dineren.
         </p>
-        <h4>Medewerkers:</h4>
+        <h3>Medewerkers:</h3>
         <MedewerkerData />
         <h2 className={"adress"}>
           Adres: Marktplein 2, Amsterdam, Netherlands
