88202a311d4733c02d9c781e86df069476f0faf1
Djoeke <djoekeduijker@live.nl>
2022-05-18 16:55:17 +0200

    code in english; text in dutch

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../css/home.css";
 import {Link} from "react-router-dom";
-import MedewerkerData from "../components/medewerkerData";
+import MedewerkerData from "../components/staffData";
 
 export default function Home() {
 
