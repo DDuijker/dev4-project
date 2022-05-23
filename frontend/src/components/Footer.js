3ed18dd02e349ee004b53e38ac8d953ea75fac00
Marjon <marjonphu15@gmail.com>
2022-05-23 13:45:59 +0200

    fixed footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "../css/footer.css"
+import "../css/footer.css";
 
 export default function Footer() {
-    return (
-        <div className={"footer"}>
-            <p>Adres: Marktplein 2, Amsterdam, Nederland</p>
-            <p>Telefoonnummer: 0621364882 <span> ○ </span> Email: gitpub@gmail.com</p>
-        </div>
-    )
+  return (
+    <div className={"footer"}>
+      <p className={"footerText"}>Adres: Marktplein 2, Amsterdam, Nederland</p>
+      <p className={"footerText"}>
+        Telefoonnummer: 0621364882 <span> ○ </span> Email: gitpub@gmail.com
+      </p>
+    </div>
+  );
 }
