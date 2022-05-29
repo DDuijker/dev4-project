409bb0cec4b5a0ed04506c7d404392358fb87cab
Floor K <98243636+killingFloor5@users.noreply.github.com>
2022-05-29 16:16:26 +0200

    dit

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,15 @@
+import sqlite3
 from flask import Flask, jsonify
+from db import DB
 
 app = Flask(__name__)
-
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect("./database/restaurant.db") 
+    except sqlite3.error as e:
+        print(e)
+    return conn
 
 @app.route('/')
 def index():
@@ -12,6 +20,14 @@ def index():
 @app.route('/members')
 def members():
     return {"members": ["Member 1", "Member 2", "Member 3"]}
+
+@app.route('/menu')
+def menu():
+    conn = db_connection()
+    qry = "SELECT * FROM menu"
+    menukaart = DB.all(qry)
+    return jsonify(menukaart)
+  
 
 
 @app.route('/home')
