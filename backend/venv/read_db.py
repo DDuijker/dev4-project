c3dc36582b34720a8924f5d661865a4f50649f61
Floor K <98243636+killingFloor5@users.noreply.github.com>
2022-05-30 12:43:58 +0200

    backend ophalen lukt

--- backend/venv/read_db.py
+++ backend/venv/read_db.py
@@ -1,12 +1,13 @@
 import sqlite3
 from flask import Flask, jsonify
 from db import DB
+from sqlite3.dbapi2 import *
 
 app = Flask(__name__)
 def db_connection():
     conn = None
     try:
-        conn = sqlite3.connect("./database/restaurant.db") 
+        conn = sqlite3.connect("./backend/database/restaurant.db") 
     except sqlite3.error as e:
         print(e)
     return conn
