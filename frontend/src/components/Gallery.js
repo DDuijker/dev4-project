7f0442d497d14ee325a32cf6ff18318441a9101c
Djoeke <djoekeduijker@live.nl>
2022-06-29 13:41:07 +0200

    gallerij title

--- frontend/src/components/Gallery.js
+++ frontend/src/components/Gallery.js
@@ -2,11 +2,12 @@ import React from "react";
 import "../css/gallerij.css";
 
 export default function Gallery(props) {
+    console.log(props);
   return (
     <div className="row">
       <div className="gallerij-card">
-        <img src={props.photo} alt="GalleryImage" />
-        <h4>{props.naam && props.naam}</h4>
+          <img src={props.photo} alt="GalleryImage"/>
+          <h4>{props.title && props.title}</h4>
       </div>
     </div>
   );
